d34e6c0a58ec03e90540a25de616495e8e68669b
nechama <nechamy5834@gmail.com>
2023-12-17 00:59:35 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(8787, () => {
+    console.log("Server started!");
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+}
+);
+
+
+//add get request that check if the request body has name = "admin" and password = 123456
+//path: localhost:8787/login
+app.post("/login", (req, res) => {
+    const body = req.body;
+    if (body.name === "admin" && body.password === "123456") {
+        res.statusCode = 200;
+        res.send("Login success!");
+    } else {
+        res.statusCode = 401;
+        res.send("Login failed!");
+    }
+});
+
+// add post request that add new appointment to the appointments array and check in the array if the time is available or not
+const appointments = [];
+
+app.post("/addAppointment", (req, res) => {
+    const body = req.body;
+    let isAvailable = true;
+    // from foreach loop to for of loop
+    for (const appointment of appointments) {
+        if (appointment.dateTime === body.dateTime) {
+            isAvailable = false;
+        }
+    }
+
+    if (isAvailable) {
+        appointments.push(body);
+        res.statusCode = 200;
+        res.send("Appointment added successfully!");
+    } else {
+        res.statusCode = 400;
+        res.send("Appointment is not available!");
+    }
+});
+
+// add get request that return all appointments
+app.get("/appointments", (req, res) => {
+    res.send(appointments);
+});
+
+let businessData ={};
+
+app.post("/businessData", (req, res) => {
+    const body = req.body;
+    businessData = body;
+    res.statusCode = 200;
+    res.send(businessData);
+});
+
+app.get("/businessData", (req, res) => {
+    res.send(businessData);
+});
---
+++ readme.md
@@ -0,0 +1,41 @@
+# Business React Project
+
+This is a simple Node.js Express server that provides several endpoints for managing appointments and user login.
+
+## Getting Started
+
+To get started, you need to have Node.js and npm installed on your machine.
+
+1. Clone the repository
+2. Install the dependencies using `npm install`
+3. Start the server using `npm start`
+
+## Endpoints
+
+### `GET /`
+
+Returns a simple "Hello World!" message.
+
+### `POST /login`
+
+Expects a JSON body with `name` and `password` fields. If `name` is "admin" and `password` is "123456", it returns a success message. Otherwise, it returns a failure message.
+
+### `POST /addAppointment`
+
+Expects a JSON body with an `dateTime` field. It checks if the time is available and if so, adds a new appointment to the appointments array and returns a success message. If the time is not available, it returns a failure message.
+
+### `GET /appointments`
+
+Returns all appointments.
+
+### `POST /businessData`
+
+Expects a JSON body with business data. It stores the data and returns it.
+
+### `GET /businessData`
+
+Returns the stored business data.
+
+## Testing
+
+You can test the endpoints using any HTTP client like curl or Postman.
